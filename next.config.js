47c9f4d750c2c2640fded4735b574240f25b1988
PanupongJJ <panupong.jangjun@gmail.com>
2025-05-15 15:07:25 +1000

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,58 +1,46 @@
 const {
   PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_BUILD,
-} = require('next/constants')
+} = require('next/constants');
 
 module.exports = (phase) => {
-  // Setting Phase
-  const isDev = phase === PHASE_DEVELOPMENT_SERVER
-  const isProd = phase === PHASE_PRODUCTION_BUILD
-  console.log(`isDev:${isDev}  isProd:${isProd}`)
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const isProd = phase === PHASE_PRODUCTION_BUILD;
+  console.log(`isDev:${isDev}  isProd:${isProd}`);
 
-  const reactStrictMode = true;
-
-  // UNCONFIGURED HOST + REMOTE PATTERNS: https://nextjs.org/docs/pages/api-reference/components/image#remotepatterns
-  const images = {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.bbci.co.uk',
-      },
-      {
-        protocol: 'http',
-        hostname: '**.bbc.co.uk',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.abc-cdn.net.au',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.**.**',
-      },
-    ]
-  }
-
-  const env = {
-    SERVER_NAME: (() => {
-      if (isDev) return 'http://localhost:3000/'
-      if (isProd) return 'https://YOUR-DOMAIN-HERE.vercel.app/'
-    })(),
-    NEWS_API_KEY: process.env.NEWS_API_KEY,
-  }
-  const nextConfig = {
-   output: 'export',
-   distDir: 'dist'
-  }
-  // Next.config returns an object
   return {
-    reactStrictMode,
-    images,
-    env,
-    nextConfig
-  }
-}
+    reactStrictMode: true,
+    output: 'export',
+    distDir: 'dist',
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**.bbci.co.uk',
+        },
+        {
+          protocol: 'http',
+          hostname: '**.bbc.co.uk',
+        },
+        {
+          protocol: 'https',
+          hostname: '**.abc-cdn.net.au',
+        },
+        {
+          protocol: 'https',
+          hostname: '**.**',
+        },
+        {
+          protocol: 'https',
+          hostname: '**.**.**',
+        },
+      ],
+    },
+    env: {
+      SERVER_NAME: isDev
+        ? 'http://localhost:3000/'
+        : 'https://YOUR-DOMAIN-HERE.vercel.app/',
+      NEWS_API_KEY: process.env.NEWS_API_KEY,
+    },
+  };
+};
